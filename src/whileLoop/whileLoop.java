646a449101da0ca22fa677cc7ce940c5ae843b11
John Hairston <j.hairston71@gmail.com>
2022-07-05 15:56:00 -0400

    for and while

---
+++ src/whileLoop/whileLoop.java
@@ -0,0 +1,21 @@
+package whileLoop;
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		// runs i first then a 
+		
+		int a = 0;
+		for (int i =0; i < 10; i++) {
+			System.out.println("i is less than 10: " + i);
+		}
+		while (a < 10) {
+			System.out.println("a is less than 10: " + a);
+			a++;
+			
+		}
+	}
+
+}
+
+
